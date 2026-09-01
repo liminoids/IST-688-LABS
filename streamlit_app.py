6523da816f51faff2f28364107aa657b60f52fac
liminoids <237880006+liminoids@users.noreply.github.com>
2026-09-01 11:01:14 -0400

    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-so all streamlit has is import streamlit as st
+import streamlit as st
+
 lab1 = st.Page('Lab1.py', title='Lab 1')
 lab2 = st.Page('Lab2.py', title='Lab 2', default=True)
+
 pg = st.navigation([lab1, lab2])
 pg.run()
